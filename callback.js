d15cf5935402c5ebdb017f4dfbe207157df3f77f
AlQasmSystem <alqasmsystem@localhost.localdomain>
2020-09-26 14:02:09 +0000

    Bismillah, Nomor 4

---
+++ callback.js
@@ -0,0 +1,16 @@
+function mentorProgrammer(nama, status, asal) {
+  nama();
+  status();
+  asal();
+}
+
+function nama() {
+  console.log("Nama Mentor Wandi Pratama");
+}
+function status() {
+  console.log("Status nya Santri");
+}
+function asal() {
+  console.log("Berasal dari Kota Prabumulih");
+}
+mentorProgrammer(nama, status, asal);
